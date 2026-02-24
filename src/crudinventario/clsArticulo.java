005c801c67c5f0cf90ce584eb4675872ec402324
Erickelpro800 <tp240112347@tepatitlan.tecmm.edu.mx>
2026-02-24 13:13:03 -0600

    Interface y objeto articulo listo

---
+++ src/crudinventario/clsArticulo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crudinventario;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class clsArticulo {
+    
+    private String codigo;
+    private String descripcion;
+    private Double precio;
+    
+    public clsArticulo(String codigo, String descripcion, Double precio){
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+    
+    public String aTexto(){
+        String articulo = this.codigo + "|" + this.descripcion + "|" + this.precio;
+        return articulo;
+    }
+    
+    public void guardar(){
+        System.out.println(this.aTexto());
+    }
+}
